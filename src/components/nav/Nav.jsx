9f73d993dfd627621db7a453249e9561b8d1e587
christopher-moses <christmoses1999@icloud.com>
2022-09-04 10:50:39 +0100

    nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import "./nav.css";
+import { AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineUser } from 'react-dom/ai';
+import { BiBook } from 'react-dom/bi';
+import { RiServiceLine } from 'react-icons/ri';
+import {TbMessage2} from 'react-dom/tb';
 
-const Nav = () => {
+
+
+const Nav = () => { 
   return (
-    <div>Nav</div>
+      <nav>
+          <a href='#'><AiOutlineHome /></a>
+          <a href='#about'><AiOutlineUser  /></a>
+          <a href='#experience'><BiBook /></a>
+          <a href='#services'><RiServiceLine /></a>
+          <a href='#contact'><TbMessage2/></a>
+      </nav>
   )
 }
 
