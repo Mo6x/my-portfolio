87f6e706b263f1ca7d8db7bcfa8890fe480e2529
christopher-moses <christmoses1999@icloud.com>
2022-09-21 06:41:12 +0100

    I install Swiper.js for my Testimonial.jsx to have a nice pagination

--- src/components/testimonials/Testimonial.jsx
+++ src/components/testimonials/Testimonial.jsx
@@ -6,6 +6,19 @@ import Avt3 from '../../assets/avatar3.jpeg';
 import Avt4 from '../../assets/avatar4.jpeg';
 
 
+
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+
 const data = [
   {
     avatar: Avt1,
@@ -37,21 +50,27 @@ const Testimonial = () => {
     <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className='container testimonials__container'>
+      <Swiper className='container testimonials__container'>
+         // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
         {
           data.map(({ avatar, name, review }, index) => {
             return (
-              <article key={index} className='testimonial'>
+              <SwiperSlide key={index} className='testimonial'>
                 <div className='client__avatar'>
                      <img src={avatar} />'
                 </div>
                 <h5 className='client__name'>{name}</h5>
                 <small className='client__review'>{ review }</small>
-            </article>
+            </SwiperSlide >
           )
           })
              }
-        </div>
+        </Swiper>
     </section>
   )
 }
