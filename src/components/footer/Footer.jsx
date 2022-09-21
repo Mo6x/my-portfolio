b27139af89c7c696f1a051c9696e2e283327ae28
christopher-moses <christmoses1999@icloud.com>
2022-09-21 13:44:35 +0100

    done with footer.jsx with little details about me

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import "./footer.css";
+import { FaLinkedin } from 'react-icons/fa';
+import { IoLogoTwitter } from 'react-icons/lo';
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'>Christ-Moses</a>
+      
+      <ul className='permalink'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#testimonials'>Testimonials</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href='https://twitter.com'>< FaLinkedin/></a>
+         <a href='https://linkedIn.com'><IoLogoTwitter/></a>
+      </div>
+
+      <div className='footer__copyright'>
+          <small>&created: Build by Christopher Moses</small>
+      </div>
+    </footer>
   )
 }
 
